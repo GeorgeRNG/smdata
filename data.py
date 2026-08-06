c5ddbdb8d2bd5a39ad926d01571c2432352fda0d
GeorgeRNG <81434111+GeorgeRNG@users.noreply.github.com>
2026-08-06 16:08:36 +0100

    rigidbody plags, fix container a little bit

--- data.py
+++ data.py
@@ -3,6 +3,21 @@ from struct import pack, unpack, calcsize
 class RigidBody:
     struct = ">2s B I H 16s H 4f 3f"
 
+    FLAG_DISABLE_BREAKING = 2**0
+    """Players cannot break blocks, uses the encryptor's hexagonal shields and sounds when trying"""
+    FLAG_DISABLE_PLACING = 2**1
+    """Players cannot place blocks, see the placing or breaking overlays, or use the weld tool (unless the creation is on a lift, in which case the weld tool can merge neighbouring pieces on joints albeit it with some missing particles)"""
+    FLAG_DISABLE_PAINTING = 2**2
+    """Players cannot use the paint gun to paint or unpaint"""
+    FLAG_DISABLE_CONNECTION = 2**3
+    """Players cannot see connections or edit connections"""
+    FLAG_DISABLE_LIFT = 2**4
+    """Players cannot put free moving creations on the lift"""
+    FLAG_DISABLE_CONFIGURE = 2**5
+    """Players cannot access the configuration or upgrade screen for parts, but can still use parts"""
+    FLAG_DISABLE_DAMAGE = 2**6
+    """Explosions, melee, and projectiles cannot damage blocks, and enemies do not consider attacking blocks"""
+
     def __init__(self, data: bytes):
         self.header: bytes
         self.a: bytes
@@ -14,21 +29,31 @@ class RigidBody:
         self.qB: float
         self.qC: float
         self.qD: float
-        """East"""
         self.x: float
-        """North"""
+        """East"""
         self.y: float
-        """Altitude"""
+        """North"""
         self.z: float
+        """Altitude"""
         self.data: RigidBodyData
         start = calcsize(self.struct)
-        (self.header,self.a,self.id,self.worldid,self.id_b,self.pad,self.qA,self.qB,self.qC,self.qD,self.x,self.y,self.z) = list(unpack(self.struct, data[ : start]))
+        (self.header,self.a,self.id,self.worldid,self.id_b,self.pad,self.qA,self.qB,self.qC,self.qD,self.x,self.y,self.z) = unpack(self.struct, data[ : start])
         if self.header == RigidBodyStatic.header: self.data = RigidBodyStatic(data[start :])
         if self.header == RigidBodyMobile.header: self.data = RigidBodyMobile(data[start :])
 
-        #                                                             [quaternion rotation              ] [xyz                     ]
-        # 0002 01 00000043 0000 c27c7d3fc289cbbec1832452c19b954c 0000 3c4e1e8f b932fa16 bbb42435 3f7ff9d2 c1857fbe c2873662 bf89a19e 00000000000000000000000000000000000000000000000000
-        # 0001 01 000000bc 0000 42b17df442b1020c42f7fdf442f7020c 0000 3f800000 00000000 00000000 00000000 42f00000 42b00000 00000000 00ffffffff
+        #                                                             [quaternion rotation              ] [xyz                     ] flags
+        # 0002 01 00000043 0000 c27c7d3fc289cbbec1832452c19b954c 0000 3c4e1e8f b932fa16 bbb42435 3f7ff9d2 c1857fbe c2873662 bf89a19e 00    000000000000000000000000000000000000000000000000
+        # 0001 01 000000bc 0000 42b17df442b1020c42f7fdf442f7020c 0000 3f800000 00000000 00000000 00000000 42f00000 42b00000 00000000 00    ffffffff
+
+        # 0001 01 00000438 0000 c0304189c03fbe77c2020419c206fbe7 0000 3f800000 00000000 00000000 00000000 c2000000 00000000 00000000 00 ffffffff # default frozen
+        # 0002 01 0000043a 0000 c08ddce1c09628ffc1f04c8ac1f844be 0000 bb1565a6 ba19839c bbc04b2c 3f7ffeb1 c1e037ec c07e28c2 be6fe468 00 000000000000000000000000000000000000000000000000 # free
+        # 0001 01 00000440 0000 c020406ac0afe573c1b00828c1c3f972 0000 3a6da6fb 3a3745b6 b8a6e3dd 3f7ffff5 c1c00128 c0800000 3c8185d0 00 ffffffff # custom frozen
+        # 0001 01 0000046f 0000 bfc08312bfdf7ceec1a60831c1abf7cf 0000 3f800000 00000000 00000000 00000000 c1a00000 00000000 00000000 01 ffffffff
+
+        # 0002010000043a0000c08ddce1c09628ffc1f04c8ac1f844be0000bb1565a6ba19839cbbc04b2c3f7ffeb1c1e037ecc07e28c2be6fe46800000000000000000000000000000000000000000000000000 # free
+        # 
+        #                                                                                                                       NN
+        # 0002010000046e00003dc25460c02f39dbc16b1e8bc1813915000037b8fb24b7b24ac63d245f6b3f7fcb36c1809d95bf7f7026bef7d86a00000000000000000000000000000000000000000000000041 mobile, encrypted
 
     def make(self) -> bytes:
         return pack(self.struct, self.header,self.a,self.id,self.worldid,self.id_b,self.pad,self.qA,self.qB,self.qC,self.qD,self.x,self.y,self.z) + self.data.make()
@@ -39,19 +64,23 @@ class RigidBodyData:
     def make(self) -> bytes:
         raise NotImplementedError()
 class RigidBodyStatic(RigidBodyData):
-    struct = "25s"
+    struct = "B 4s"
     header = b"\x00\x01"
     def __init__(self, data):
-        self.data: bytes = data
+        self.flags: int
+        self.data: bytes
+        (self.flags,self.data) = unpack(self.struct,data)
     def make(self):
-        return self.data
+        return pack(self.struct, self.flags, self.data)
 class RigidBodyMobile(RigidBodyData):
-    struct = "5s"
+    struct = "24s B"
     header = b"\x00\x02"
     def __init__(self, data):
-        self.data: bytes = data
+        self.data: bytes
+        self.flags: int
+        (self.data, self.flags) = unpack(self.struct,data)
     def make(self):
-        return self.data
+        return pack(self.struct, self.data, self.flags)
 
 """
 Every part, wedge, and stretch of blocks is its own ChildShape
@@ -172,22 +201,21 @@ class Container:
 
     def __init__(self, data):
         self.header: ContainerHeader
-        self.items: list[Item]
+        self.items: list[Item] = []
 
-        offset = calcsize(self.header.size)
-        self.header = Container(data[slice(offset)])
+        offset = calcsize(ContainerHeader.struct)
+        self.header = ContainerHeader(data[:offset])
         size = self.header.size
         for _ in range(size):
             end = offset + calcsize(Item.struct)
-            item = Item(data[slice(offset, end)])
+            item = Item(data[offset:end])
             self.items.append(item)
             offset = end
 
-    def make_container(self):
+    def make(self):
         assert len(self.items) == self.header.size, f"len(self.items) = {len(self.items)} but it should be {self.header.size}"
         container = self.header.make()
         for item in self.items:
-            assert(len(item) == calcsize(Item.struct))
-            container += item
+            container += item.make()
         container += b"\x00\x00"
         return container
